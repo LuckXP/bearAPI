f3d43273110f2c32de3bbc423a2fd55653c014cc
LuckXP <js.sindler@gmail.com>
2016-02-15 12:02:01 -0700

    add get with dummy data

--- server.js
+++ server.js
@@ -1,8 +1,40 @@
 var express = require('express');
 var app = express();
 
+var becky = {
+	name: 'Becky',
+	id: 'Brown',
+	age: 38,
+	gender: 'Female'
+}
+
+var jesse = {
+	name: 'Jesse',
+	id: 'Brown',
+	age: 27,
+	gender: 'Male'
+}
+
+var jabob = {
+	name: 'Jacob',
+	id: 'Brown',
+	age: 12,
+	gender: 'Male'
+}
+
+var monte = {
+	name: 'Monte',
+	id: 'Brown',
+	age: 43,
+	gender: 'Male'
+}
+
+var bears =[becky, jesse, jabob, monte]
+
+
 app.get('/api/bears', function(req, res) {
-	res.json({title: 'does it work?', body: 'I hope so..'})
+	
+	res.json(bears)
 });
 
 app.listen(3000, function() {

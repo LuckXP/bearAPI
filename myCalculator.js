acfbc8ea5ad5d810d0177ec38422ccc5b32e79bf
LuckXP <js.sindler@gmail.com>
2016-02-15 11:10:54 -0700

    add express and gitignore file for modules

--- myCalculator.js
+++ myCalculator.js
@@ -3,3 +3,5 @@ var Calculator = require('./calculator');
 var myCalculator = new Calculator();
 console.log(myCalculator.sum(4,5));
 console.log(myCalculator['sum'](6,7));
+console.log(myCalculator.subtract(4,5));
+console.log(myCalculator['subtract'](6,7));
